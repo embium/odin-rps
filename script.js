1758eac98f8a16f0fe5f069acf94e763381bac4f
Michael Mooney <mikeymooney1991@gmail.com>
2023-05-28 19:54:47 -0500

    Formatting

--- script.js
+++ script.js
@@ -12,49 +12,48 @@ function playRound(playerSelection) {
     let results;
     let won = false;
     let tied = false;
-
     let computerSelection = getComputerChoice();
 
-    if(playerSelection == computerSelection) {
+    if (playerSelection == computerSelection) {
         results = "It's a tie!";
         tied = true;
     }
 
-    if(computerSelection == "rock") {
-        if(playerSelection == "paper") {
-            results =`You win, ${playerSelection} beats ${computerSelection}!`;
-            won = true;
-        }
-
-        if(playerSelection == "scissors") {
-            results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
-        }
-    }
-
-    else if(computerSelection == "paper") {
-        if(playerSelection == "scissors") {
+    if (computerSelection == "rock") {
+        if (playerSelection == "paper") {
             results = `You win, ${playerSelection} beats ${computerSelection}!`;
             won = true;
         }
 
-        if(playerSelection == "rock") {
+        if (playerSelection == "scissors") {
             results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
         }
     }
 
-    else if(computerSelection == "scissors") {
-        if(playerSelection == "rock") {
+    else if (computerSelection == "paper") {
+        if (playerSelection == "scissors") {
             results = `You win, ${playerSelection} beats ${computerSelection}!`;
             won = true;
         }
 
-        if(playerSelection == "paper") {
+        if (playerSelection == "rock") {
             results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
         }
     }
-    
-    if(!tied){
-        if(won){
+
+    else if (computerSelection == "scissors") {
+        if (playerSelection == "rock") {
+            results = `You win, ${playerSelection} beats ${computerSelection}!`;
+            won = true;
+        }
+
+        if (playerSelection == "paper") {
+            results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
+        }
+    }
+
+    if (!tied) {
+        if (won) {
             wins++;
         } else {
             losses++;
@@ -62,8 +61,10 @@ function playRound(playerSelection) {
 
         tries++
     }
+
     document.getElementById("results").innerHTML = results + ` ${wins}/${losses}<div class="tries">${tries} out of 5 tries left!</div>`;
-    if(tries >= 5) {
+
+    if (tries >= 5) {
         tries = 0;
         wins = 0;
         losses = 0;
