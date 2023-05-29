716f876aa9f8368f2c5e55cf7f335d00ce82a8e5
Michael Mooney <mikeymooney1991@gmail.com>
2023-05-28 19:52:42 -0500

    Don't count as a try if it's a tied game.

--- script.js
+++ script.js
@@ -10,47 +10,58 @@ function getComputerChoice() {
 
 function playRound(playerSelection) {
     let results;
-    tries++
+    let won = false;
+    let tied = false;
 
     let computerSelection = getComputerChoice();
 
-    if(playerSelection == computerSelection) results = "It's a tie game!";
+    if(playerSelection == computerSelection) {
+        results = "It's a tie game!";
+        tied = true;
+    }
 
     if(computerSelection == "rock") {
         if(playerSelection == "paper") {
             results =`You win, ${playerSelection} beats ${computerSelection}!`;
-            wins++;
+            won = true;
         }
+
         if(playerSelection == "scissors") {
             results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
-            losses++;
         }
     }
 
     else if(computerSelection == "paper") {
         if(playerSelection == "scissors") {
             results = `You win, ${playerSelection} beats ${computerSelection}!`;
-            wins++;
+            won = true;
         }
 
         if(playerSelection == "rock") {
             results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
-            losses++;
         }
     }
 
     else if(computerSelection == "scissors") {
         if(playerSelection == "rock") {
             results = `You win, ${playerSelection} beats ${computerSelection}!`;
-            wins++;
+            won = true;
         }
 
         if(playerSelection == "paper") {
             results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
-            losses++;
         }
     }
     
+    if(!tied){
+        if(won){
+            wins++;
+        } else {
+            losses++;
+        }
+
+        tries++
+    }
     document.getElementById("results").innerHTML = results + ` ${wins}/${losses}<div class="tries">${tries} out of 5 tries left!</div>`;
     if(tries >= 5) {
         tries = 0;
