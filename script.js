3973e327a6eaa7c6b24230efdd636617658fb573
Michael Mooney <mikeymooney1991@gmail.com>
2023-05-28 19:45:35 -0500

    Add tries + wins/losses!

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let tries = 0;
+let wins = 0;
+let losses = 0;
+
 function getComputerChoice() {
     choices = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * choices.length);
@@ -6,24 +10,51 @@ function getComputerChoice() {
 
 function playRound(playerSelection) {
     let results;
+    tries++
+
     let computerSelection = getComputerChoice();
 
     if(playerSelection == computerSelection) results = "It's a tie game!";
 
     if(computerSelection == "rock") {
-        if(playerSelection == "paper") results =`You win, ${playerSelection} beats ${computerSelection}!`;
-        if(playerSelection == "scissors") results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
+        if(playerSelection == "paper") {
+            results =`You win, ${playerSelection} beats ${computerSelection}!`;
+            wins++;
+        }
+        if(playerSelection == "scissors") {
+            results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
+            losses++;
+        }
     }
 
-    if(computerSelection == "paper") {
-        if(playerSelection == "scissors") results = `You win, ${playerSelection} beats ${computerSelection}!`;
-        if(playerSelection == "rock") results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
+    else if(computerSelection == "paper") {
+        if(playerSelection == "scissors") {
+            results = `You win, ${playerSelection} beats ${computerSelection}!`;
+            wins++;
+        }
+
+        if(playerSelection == "rock") {
+            results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
+            losses++;
+        }
     }
 
-    if(computerSelection == "scissors") {
-        if(playerSelection == "rock") results = `You win, ${playerSelection} beats ${computerSelection}!`;
-        if(playerSelection == "paper") results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
-    }
+    else if(computerSelection == "scissors") {
+        if(playerSelection == "rock") {
+            results = `You win, ${playerSelection} beats ${computerSelection}!`;
+            wins++;
+        }
 
-    document.getElementById("results").innerHTML = results;
+        if(playerSelection == "paper") {
+            results = `Computer wins, ${computerSelection} beats ${playerSelection}!`;
+            losses++;
+        }
+    }
+    
+    document.getElementById("results").innerHTML = results + ` ${wins}/${losses}<div class="tries">${tries} out of 5 tries left!</div>`;
+    if(tries >= 5) {
+        tries = 0;
+        wins = 0;
+        losses = 0;
+    }
 }
